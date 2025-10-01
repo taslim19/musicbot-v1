cfecd2d0f117169b7bf2f590dd72d750488c4d93
taslim19 <ataslimx@gmail.com>
2025-10-01 14:10:50 +0530

    fixed:z

--- DragMusic/utils/stream/stream.py
+++ DragMusic/utils/stream/stream.py
@@ -11,7 +11,7 @@ from DragMusic.misc import db
 from DragMusic.utils.database import add_active_video_chat, is_active_chat
 from DragMusic.utils.exceptions import AssistantErr
 from DragMusic.utils.inline import aq_markup, close_markup, stream_markup
-from DragMusic.utils.pastebin import AnonyBin
+from DragMusic.utils.pastebin import DragBin
 from DragMusic.utils.stream.queue import put_queue, put_queue_index
 from DragMusic.utils.thumbnails import get_thumb
 
